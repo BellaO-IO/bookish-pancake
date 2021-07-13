55eefacc27866ded8cb0df3171a375ac709ae691
bella <theboss@ciaobella.me>
2021-07-13 14:58:24 -0700

    ada

---
+++ KanyeWestAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWestAPI
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            QuoteThing.KanyeQuote();
+            QuoteThing.RonQuote();
+        }
+        
+            
+            
+
+    }
+}
